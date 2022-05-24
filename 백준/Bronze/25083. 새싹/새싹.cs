3dfde67cdbd9161423fd1f1877e083198c525f3b
Jihoon Song <43688074+jh2song@users.noreply.github.com>
2022-05-24 13:08:14 +0900

    [Bronze V] Title: 새싹, Time: 56 ms, Memory: 5292 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,22 @@
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
+using System;
+
+namespace Baekjoon {
+    class Program {
+        static void Main() {
+            Console.WriteLine("         ,r'\"7");
+            Console.WriteLine("r`-_   ,'  ,/");
+            Console.WriteLine(" \\. \". L_r'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+        }
+    }
+}
